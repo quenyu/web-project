bed369e241fea96655a32f661a52bb0df3e34693
Hatsune <keyfknblade@gmail.com>
2024-07-04 23:38:47 +0300

    added jest, added test to classNames helper

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,16 @@
+import { classNames } from "./classNames"
+
+describe("classNames", () => {
+  test("with only first param", () => {
+    expect(classNames("some class")).toBe("some class")
+  })
+  test("with additional param", () => {
+    expect(classNames("some class", {}, ["class1", "class2"])).toBe("some class class1 class2")
+  })
+  test("with mods param", () => {
+    expect(classNames("some class", { "scrollable": true, "fixable": true }, ["class1", "class2"])).toBe("some class class1 class2 scrollable fixable")
+  })
+  test("with undefined mods param", () => {
+    expect(classNames("some class", { "scrollable": undefined }, [])).toBe("some class")
+  })
+})
