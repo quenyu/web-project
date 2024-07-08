ccfe06fd5301ed9570e3e4e8b36670fd740f445e
Hatsune <keyfknblade@gmail.com>
2024-07-08 22:14:17 +0300

    added rules for eslint overrides test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,9 +38,24 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'i18next/no-literal-string': [
+      'error', {
+        markupOnly: true,
+        ignoreAttribute: ["data-testid"]
+      },
+    ],
   },
   globals: {
     isDev: true,
   },
+  overrides: [
+    {
+      files: [
+        "*.test.{ts,tsx}"
+      ],
+      rules: {
+        'i18next/no-literal-string': 'off'
+      }
+    }
+  ]
 };
