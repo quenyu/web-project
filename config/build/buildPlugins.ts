f6bf97201d9c50317aa7aa1f44fc0f6c1529fe17
Hatsune <keyfknblade@gmail.com>
2024-07-14 17:43:28 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,11 +1,13 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack';
+import {
+	DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance,
+} from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new HtmlWebpackPlugin({
 			template: paths.html,
 		}),
@@ -17,8 +19,14 @@ export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInsta
 		new DefinePlugin({
 			Dev: JSON.stringify(isDev),
 		}),
-		new BundleAnalyzerPlugin({
-			openAnalyzer: false,
-		}),
 	];
+
+	if (isDev) {
+		plugins.push(new HotModuleReplacementPlugin());
+		plugins.push(new BundleAnalyzerPlugin({
+			openAnalyzer: false,
+		}));
+	}
+
+	return plugins;
 }
